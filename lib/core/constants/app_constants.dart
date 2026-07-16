30f7cda854bb867f6dc0f0a7316c23f3b8cd5d63
Hidayattt24 <hidayat.22@mhs.usk.ac.id>
2026-07-16 20:54:40 +0700

    feat: add environment configuration support and initialize application constants

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,7 +1,6 @@
 abstract final class AppConstants {
   AppConstants._();
 
-  static const String baseUrl = 'https://api.dsmes-aceh.id/api/v1';
   static const Duration connectTimeout = Duration(seconds: 15);
   static const Duration receiveTimeout = Duration(seconds: 15);
 
